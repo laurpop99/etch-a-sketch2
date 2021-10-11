42c525505d2846de888a54523030dd29f5174ab8
Popescu Laurentiu <laurpop99@yahoo.com>
2021-10-12 01:48:00 +0300

    Added functionality to the buttons and the grid.

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
-const GRIDSIZE = 16;
+let size = 16;
+let newSize = size;
 const container = document.querySelector(".container");
 
-function createGrid()
-{
-        for(let i = 1; i <= GRIDSIZE;i++)
-        {
-            for(let x = 1; x <= GRIDSIZE; x++)
-            {
-                const div = document.createElement("div");
-                div.classList.add("divs");
-                container.appendChild(div);
-                
-            }
-        }
+let myDivs = [];
+
+function createGrid(size){
 
 
+    for(let i = 1; i <= size;i++){
+                for(let x = 1; x <= size; x++){
+                    const div = document.createElement("div");
+                    div.style.width = (992- size*2)/size +"px";
+                    div.style.height = (992- size*2)/size +"px";
+                    myDivs.push(div);
+                    div.classList.add("divs");
+                    container.appendChild(div);
+                    div.addEventListener("mouseover", function(e){
+                        e.target.style.backgroundColor = `rgb(${randomize(255)},${randomize(255)},${randomize(255)},1)`;
+                    })
+                    
+                }
+    }
+} 
+
+function removeGrid(){
+    myDivs.forEach( (div) => div.remove());
+}       
+
+function chooseSize(){
+    newSize = prompt("Enter number of squares per row");
+    if (newSize > 100){
+        newSize = 100;
+        alert("You can't choose over 100 rows!");
+    }
+    removeGrid();
+    createGrid(newSize);
 }
 
-createGrid();
+function randomize(num){
+ return Math.floor(Math.random()*num+1);
+}
+
+
+const clearButton = document.querySelector(".clear");
+const sizeButton = document.querySelector(".size");
+sizeButton.addEventListener("click", chooseSize);
+clearButton.addEventListener("click", function(){
+    removeGrid();
+    createGrid(newSize);
+});
+
+createGrid(size);
+
